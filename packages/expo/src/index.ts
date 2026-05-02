356e6c13d4c9850e34f1cdf0c043f35ddcf9d4ab
equationalapplications <info@equationalapplications.com>
2026-05-02 08:36:36 -0400

    review: add .npmrc hoisting, re-export react hooks from expo, enable treeshake

--- packages/expo/src/index.ts
+++ packages/expo/src/index.ts
@@ -3,6 +3,7 @@ import { WikiMemory, type WikiOptions } from '@eq/wiki-core';
 import { createExpoAdapter } from './adapter';
 
 export * from '@eq/wiki-core';
+export * from '@eq/wiki-react';
 
 export function createWiki(db: SQLite.SQLiteDatabase, options: WikiOptions): WikiMemory {
   return new WikiMemory(createExpoAdapter(db), options);
